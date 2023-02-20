914064012c1242a596b3f065de55fc3948e69701
Dario Mariani <dario.mariani@hotmail.it>
2023-02-20 01:14:31 +0100

    User service repository successfully integrated

--- src/test/java/services/UserServiceTest.java
+++ src/test/java/services/UserServiceTest.java
@@ -2,38 +2,66 @@ package services;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import models.User;
-import testdataset.TestUserDataset;
+import repository.impl.UserRepositoryImpl;
 
 public class UserServiceTest {
 	
 	private UserService userService;
 	
+	@Mock
+	private UserRepositoryImpl userRepository;
+	
 	@Before
 	public void setup() {
-		var userRepository = new ArrayList<User>();
+		MockitoAnnotations.openMocks(this);
 		this.userService = new UserService(userRepository);
 	}
 
 	@Test
-	public void testCreateUserWithSameUsernameAsAnExistingThrowsIllegalArgumentException() {
-		var newUser1 = new User(TestUserDataset.USERNAME1);
-		userService.createUser(newUser1);
-		var newUser2 = new User(TestUserDataset.USERNAME1);
-		assertThrows(String.format("A User with the Username {0} already exists.", TestUserDataset.USERNAME1), IllegalArgumentException.class, () -> userService.createUser(newUser2));
-	}
-	
-	@Test
-	public void testCreateUserWhenRepositoryIsEmptyContainsOneElement() {
-		var newUser = new User(TestUserDataset.USERNAME1);
-		userService.createUser(newUser);
-		assertEquals(1, userService.findAll().size());
-	}
+    public void testFindAll() {
+        List<User> userList = new ArrayList<>();
+        userList.add(new User("John"));
+        userList.add(new User("Jane"));
+        when(userRepository.findAll()).thenReturn(userList);
+        List<User> result = userService.findAll();
+        assertEquals(2, result.size());
+        verify(userRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void testCreateUser() {
+        User newUser = new User("John");
+        when(userRepository.findAll()).thenReturn(new ArrayList<>());
+        userService.createUser(newUser);
+        verify(userRepository, times(1)).save(newUser);
+    }
+
+    @Test
+    public void testCreateUserWithExistingUsername() {
+        User existingUser = new User("John");
+        User newUser = new User("John");
+        List<User> userList = new ArrayList<User>();
+        userList.add(existingUser);
+        when(userRepository.findAll()).thenReturn(userList);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            userService.createUser(newUser);
+        });
+        assertEquals("A User with the Username John already exists.", exception.getMessage());
+        verify(userRepository, never()).save(newUser);
+    }
 
 }
